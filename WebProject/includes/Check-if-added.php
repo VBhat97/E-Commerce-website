89b35a2d21e947868664f6391772bd315a2310af
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2017-06-27 05:56:15 +0530

    Add files via upload

--- WebProject/includes/Check-if-added.php
+++ WebProject/includes/Check-if-added.php
@@ -1,8 +1,17 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+function check_if_added_to_cart ($item_id)
+{
+    include 'common.php';
+    $query1 ="SELECT * FROM user_items WHERE item_id='$item_id' AND user_id ='$user_id' and status='Added to cart'";
+    $user_query_result = mysqli_query($con, $query1);
+    
+    if (mysqli_num_rows($user_query_result)>=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
